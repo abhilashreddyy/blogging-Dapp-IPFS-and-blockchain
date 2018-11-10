e0497c37439173b774c3c6694bfdc36c28aa336f
abhilash reddy <abhilashreddyy1@gmail.com>
2018-11-10 17:45:53 +0530

    changed parameters,js file

--- public/js/params.js
+++ public/js/params.js
@@ -1,5 +1,5 @@
 params = {
-  password : "1220047c3c19",
+  password : "***************", // add your password
   account_index : 0,
-  address : "0x2d620b26adc094280c3d404d324fa397da1f8031",
+  address : "0x2d620b26adc094280c3d404d324fa397da1f8031", // add your address
 }
